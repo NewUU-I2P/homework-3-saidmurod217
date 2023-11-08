85db4b102666d0caa75c22bc177cb82d6965e5b6
Saidmurod Khamidov <saidmurod217@gmail.com>
2023-11-08 23:13:22 +0500

    asdf

--- main.cpp
+++ main.cpp
@@ -8,15 +8,6 @@
 using namespace std;
 
 int main() {
-    cout << "Problem 1\n";
-    problemSolution1();
-    cout << "Problem 2\n";
-    problemSolution2();
-    cout << "Problem 3\n";
-    problemSolution3();
-    cout << "Problem 4\n";
-    problemSolution4();
-    cout << "Problem 5\n";
-    problemSolution5();
+
     return 0;
 }
--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,29 +1,6 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    float consumption = consumed_water;
-    float totalCost = 0;
-    cost = totalCost;
-    if (consumption <= 30) {
-        totalCost += consumption * (float)0.4;
-    } else {
-        totalCost += 30 * 0.4;
-        consumption -= 30;
 
-        if (consumption <= 20) {
-            totalCost += consumption * (float)0.12;
-        } else {
-            totalCost += 20 * (float)0.12;
-            consumption -= 20;
-
-            if (consumption <= 10) {
-                totalCost += consumption * (float)1.4;
-            } else {
-                totalCost += 10 * 1.4;
-                consumption -= 10;
-                totalCost += consumption * (float)1.5;
-            }
-        }
-    }
     return cost;
 }
