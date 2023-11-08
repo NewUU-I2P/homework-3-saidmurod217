55235395463c208110ed797a919b679f10ac00f8
Saidmurod Khamidov <saidmurod217@gmail.com>
2023-11-08 23:07:00 +0500

    5th problem

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,5 +1,18 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
+   switch (operation) {
+       case '+':
+           result = x + y;
+           break;
+       case '-':
+           result = x - y;
+           break;
+       case '/':
+           result = x / y;
+           break;
+       case '*':
+           result = x * y;
+   }
    return result;
 }
