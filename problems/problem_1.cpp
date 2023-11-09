fc053c1cd3342e2b91a30da8a128969ef101256c
Saidmurod Khamidov <saidmurod217@gmail.com>
2023-11-09 19:27:51 +0500

    plejuarizm

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,29 +1,26 @@
-void problemSolution1(float consumed_water) {
-    float cost;
+float problemSolution1(float consumed_water) {
+    float cost = 13.0;
     // write your code here
-    float consumption = consumed_water;
-    float totalCost;
-    cost = totalCost;
-    if (consumption <= 30) {
-        totalCost += consumption * (float)0.4;
+    if (consumed_water <= 30.0) {
+        cost += consumed_water * 0.4;  // $0.4 per cubic meter for the first 30 cubic meters
     } else {
-        totalCost += 30 * 0.4;
-        consumption -= 30;
+        cost += 30.0 * 0.4;
 
-        if (consumption <= 20) {
-            totalCost += consumption * (float)0.12;
+        if (consumed_water <= 50.0) {
+            cost += (consumed_water - 30.0) * 0.12;  // $0.12 per cubic meter for the next 20 cubic meters
         } else {
-            totalCost += 20 * (float)0.12;
-            consumption -= 20;
+            cost += 20.0 * 0.12;
 
-            if (consumption <= 10) {
-                totalCost += consumption * (float)1.4;
+            if (consumed_water <= 60.0) {
+                cost += (consumed_water - 50.0) * 1.4;  // $1.4 per cubic meter for the next 10 cubic meters
             } else {
-                totalCost += 10 * 1.4;
-                consumption -= 10;
-                totalCost += consumption * (float)1.5;
+                cost += 10.0 * 1.4;
+
+                cost += (consumed_water - 60.0) * 1.5;  // $1.5 per cubic meter for every additional cubic meter
             }
         }
     }
+
+
     return cost;
 }
